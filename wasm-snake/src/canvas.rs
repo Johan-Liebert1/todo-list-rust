c9c7b985caa6c37aee2d54fcbf4b01a6647bf0b4
Johan-Liebert1 <pragyanpoudyal41999@gmail.com>
2021-12-11 13:25:22 +0530

    canvas and direction module

---
+++ wasm-snake/src/canvas.rs
@@ -0,0 +1,65 @@
+use stdweb;
+use stdweb::traits::*;
+use stdweb::unstable::TryInto;
+use stdweb::web::html_element::CanvasElement;
+use stdweb::web::{document, CanvasRenderingContext2d, Document};
+
+pub struct Canvas {
+    pub canvas: CanvasElement,
+    pub ctx: CanvasRenderingContext2d,
+    scaled_width: u32,
+    scaled_height: u32,
+    width: u32,
+    height: u32,
+}
+
+impl Canvas {
+    pub fn new(attr_id: &str, width: u32, height: u32) -> Canvas {
+        let canvas: CanvasElement = stdweb::unstable::TryInto::try_into(
+            document().query_selector(attr_id).unwrap().unwrap(),
+        )
+        .unwrap();
+
+        let ctx: CanvasRenderingContext2d = canvas.get_context().unwrap();
+
+        let scaled_width = canvas.width() / width;
+        let scaled_height = canvas.height() / height;
+
+        Canvas {
+            canvas,
+            ctx,
+            scaled_width,
+            scaled_height,
+            width,
+            height,
+        }
+    }
+
+    pub fn draw(&self, x: u32, y: u32, color: &str) {
+        assert!(x < self.width);
+        assert!(y < self.height);
+
+        self.ctx.set_fill_style_color(color);
+
+        let x = x * self.scaled_width;
+        let y = y * self.scaled_height;
+
+        self.ctx.fill_rect(
+            x as f64,
+            y as f64,
+            self.scaled_width as f64,
+            self.scaled_height as f64,
+        );
+    }
+
+    pub fn clear_all(&self) {
+        self.ctx.set_fill_style_color("white");
+
+        self.ctx.fill_rect(
+            0.0,
+            0.0,
+            self.canvas.width() as f64,
+            self.canvas.height() as f64,
+        );
+    }
+}
