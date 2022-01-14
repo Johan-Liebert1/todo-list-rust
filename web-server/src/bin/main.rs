3660b94af31f6376bc10236603a08701abfc1f03
Johan-Liebert1 <pragyanpoudyal41999@gmail.com>
2022-01-14 21:20:32 +0530

    multithreaded web server

--- web-server/src/bin/main.rs
+++ web-server/src/bin/main.rs
@@ -2,18 +2,23 @@ use std::{
     fs,
     io::{Read, Write},
     net::{TcpListener, TcpStream},
+    thread, time,
 };
+
+use web_server::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:5000").unwrap();
+    let thread_pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream);
+        // handle_single_threaded_connection(stream);
+        thread_pool.execute(|| handle_single_threaded_connection(stream));
     }
 }
 
-fn handle_connection(mut stream: TcpStream) {
+fn handle_single_threaded_connection(mut stream: TcpStream) {
     let mut buffer = [0_u8; 1024];
 
     stream.read(&mut buffer).unwrap();
@@ -21,9 +26,13 @@ fn handle_connection(mut stream: TcpStream) {
     println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 
     let get = b"Request: GET / HTTP/1.1";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
         ("HTTP/1.1 200 OK", "src/html/index.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(time::Duration::from_secs(5));
+        ("HTTP/1.1 404 NOT FOUND", "src/html/index.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND", "src/html/404.html")
     };
